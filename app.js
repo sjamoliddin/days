9157192da6d4da3e8d7ede0b8327d6cad32223cf
sjamoliddin <sjamolbek@mail.ru>
2021-02-02 21:22:40 +0500

    animate images & texts

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 const navBar = document.querySelector('.nav-bar');
 const navLinks = navBar.querySelectorAll('a');
 const contact = navBar.querySelector('.contact a');
+let controller;
+let slideScene;
+let pageScene;
+let detailScene;
 
 // Functions
 function changeBackground() {
 	navLinks.forEach((link, index) => {
 		link.addEventListener('mouseover', () => {
+			// change background and color of the links
+			// according to the position of them
 			if (index === 0) {
 				navBar.style.background = '#1da1f2';
-				link.style.color = '#fff';
-				navLinks[1].style.color = '#000';
-				navLinks[2].style.color = '#000';
-				contact.style.color = '#000';
+				link.style.color = '#fff'; // change
+				navLinks[1].style.color = '#000'; // reset
+				navLinks[2].style.color = '#000'; // reset
+				contact.style.color = '#000'; // reset
 			}
 			if (index === 1) {
 				navBar.style.background = '#FF0000';
@@ -23,7 +29,6 @@ function changeBackground() {
 			}
 			if (index === 2) {
 				navBar.style.background = '#833AB4';
-				//#833AB4
 				navLinks[0].style.color = '#000';
 				navLinks[1].style.color = '#000';
 				link.style.color = '#fff';
@@ -41,4 +46,32 @@ function changeBackground() {
 	});
 }
 
+function animateSlides() {
+	// selectors
+	const slides = document.querySelectorAll('.slide');
+	const nav = document.querySelector('.nav-header');
+
+	// loop over each slide
+	slides.forEach((slide, index, slides) => {
+		const revealImg = slide.querySelector('.reveal-img');
+		const img = slide.querySelector('img');
+		const revealText = slide.querySelector('.reveal-text');
+
+		const slideTimeline = gsap.timeline({
+			defaults: { duration: 1, ease: 'power2.inOut' },
+		});
+
+		// animate images
+		slideTimeline.fromTo(revealImg, { x: '0%' }, { x: '100%' });
+		slideTimeline.fromTo(img, { scale: '1.5' }, { scale: '1' }, '-=1');
+
+		//animate texts
+		slideTimeline.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.75');
+
+		// animate nav-header
+		slideTimeline.fromTo(nav, { y: '-200%' }, { y: '0%' }, '-=1');
+	});
+}
+
 changeBackground();
+animateSlides();
